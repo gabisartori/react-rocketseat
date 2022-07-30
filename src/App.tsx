61df36681bfa441341af7a8e422151ef7bcaccd2
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-07-30 16:23:47 -0300

    useState
    Criando e alterando valores de uma lista de estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import { Tweet } from './components/Tweet';
 
 function App() {
 // <Component atributos=qualquer_coisa />
+  function adicionarTweet(){
+    const input = document.getElementById('tweetar') as HTMLInputElement;
+    const value = input?.value;
+    setTweets([...tweets, value]);
+  };
+  const [tweets, setTweets] = useState<string[]>([
+    'Oi',
+    'Tudo bem?',
+    'Tudo sim',
+    'Que bom então'
+  ]);
+
   return (
     <div>
-      <Tweet text="oi" />
-      <Tweet text="odeio esse site"/>
+      {tweets.map((tweet)=>{
+        return <Tweet text={tweet}/>;
+      })}
+      <input type="text" name="bla" id="tweetar" />
+      <button onClick={adicionarTweet}>Adicionar tweet</button>
     </div>
   );
 }
